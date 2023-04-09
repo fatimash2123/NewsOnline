bc0a522b40375cffd224de0aed15283860e814d6
fatima2123 <fshahzad2123@gmail.com>
2023-04-10 02:14:32 +0500

    pren next buttons replaced with infinite scroll

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -11,7 +11,7 @@ export default class NewsItem extends Component {
                     className="card-img-top" alt="..."></img>
                 <div className="card-body d-flex flex-column justify-content-center align-items-center" style={{ width:"100%" }}>
                     <h5 className="card-title " style={{ height: 100,width:"100%", overflow:"hidden" }}>{title}</h5> 
-                    <h6 className="card-title" style={{padding:3, overflow:"hidden" }}><span className='position-absolute top-0 bg-danger rounded-pill start-100 badge translate-middle'>{source}</span></h6> 
+                    <div className="card-title" style={{display:"flex",justifyContent:"flex-end",position:"absolute",right:0,top:0 }}><span className='bg-danger rounded-pill  badge'>{source}</span></div> 
                     <p className="card-text" style={{ height: 120,width:"100%", overflow:"hidden", textOverflow:"ellipsis" }}>{description}</p>
                     <p className='cart-text'><small>Author: {!author?"Unknown":author}</small></p>
                     <p className='cart-text'><small>Date: {!publishedAt?"Unknown":publishedAt}</small></p>
